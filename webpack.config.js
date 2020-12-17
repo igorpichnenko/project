f7ebb35665def01d77bf16defe597fda07f045d2
Igor <igorpichnenko@gmail.com>
2020-12-18 00:08:17 +0600

    create new project

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+// модули --- ***
+module.exports = {
+	entry: {
+	  'index': './src/pages/index/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', {
+					loader: 'postcss-loader',
+					options: {
+						plugins: () => autoprefixer({
+							overrideBrowserslist: ['last 4 versions', '> 1%']
+						})
+					}
+				}, 'sass-loader']
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: {
+					pretty: true
+				}
+			},
+			{
+				test: /\.(jpg|png|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'images/[name].[ext]'
+				}
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'fonts/[name].[ext]'
+				}
+			}
+		]
+	},
+	
+	// Плагины *** ---
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+		new HtmlWebpackPlugin({
+			inject: false,
+			hash: true,
+			template: './src/pages/index/index.pug',
+			filename: 'index.html'
+		}),
+	],
+	// Стартовая страница
+	devtool: 'inline-source-map',
+	devServer: {
+		stats: 'errors-only',
+		index: 'index.html',
+		open: true,
+	}
+};
