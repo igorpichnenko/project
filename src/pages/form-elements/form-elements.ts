49ac4d49b3760c2bdb974b9b2c8e2c89d0936117
Igor <igorpichnenko@gmail.com>
2021-01-17 11:42:39 +0600

    remade the input mixin by bam, rendered part of the pug to json

--- src/pages/form-elements/form-elements.ts
+++ src/pages/form-elements/form-elements.ts
@@ -7,11 +7,11 @@ import '../../components/range-slider/range-slider.ts'
 import '../../components/pagination/pagination.ts'
 import '../../components/checkbox/checkbox.ts'
 import '../../components/like/like.ts'
-
-
-
-
 import IMask from 'imask';
+
+
+
+
 
 
 let dateMask = IMask(
@@ -25,31 +25,3 @@ let dateMask = IMask(
 
 
 
-declare global {
-    interface Window {
-       JustValidate :any;
-    }
-}
-
-
-
-function validateForms(selector:any, rules:any) {
-    new window.JustValidate(selector, {
-        rules: rules,
-        submitHandler: function (form:any, values:any, ajax:any) {
-
-            let formData = new FormData(form);
-
-            fetch("mail.php", {
-                method: "POST",
-                body: formData
-            })
-            .then(function(data) {
-                alert('Спасибо за подписку!');
-                form.reset();
-            });
-        }
-    });
-}
-
-validateForms('.column-one__form-mask', { email: { required: true, email: true } });
