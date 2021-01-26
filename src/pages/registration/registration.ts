b53ac5ec3d567db078656f684ff32a7a1ebc4f89
Igor <igorpichnenko@gmail.com>
2021-01-26 19:02:36 +0600

    refactoring code

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -3,4 +3,4 @@ import '../../scss/main.scss';
 import './registration.scss';
 import '../../../pixel';
 import '../../blocks/burger/burger';
-import '../../blocks/logic-room-reg/logic-room-reg'
+import '../../blocks/logic-room-reg/logic-room-reg';
--- src/pages/room-detalis/room-detalis.ts
+++ src/pages/room-detalis/room-detalis.ts
@@ -4,4 +4,4 @@ import '../../../pixel';
 import '../../blocks/pie/pie';
 import '../../blocks/dropdown/dropdown-init';
 import '../../blocks/burger/burger';
-import '../../blocks/calendar/calendar'
+import '../../blocks/calendar/calendar';
