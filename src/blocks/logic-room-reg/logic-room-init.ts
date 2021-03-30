03443a7b3bc9516759124df90ca3fee8d83e13ce
igorpichnenko <igorpichnenko@gmail.com>
2021-03-30 00:10:52 -0400

    #7 return logic to block

--- src/blocks/logic-room-reg/logic-room-init.ts
+++
@@ -1,5 +0,0 @@
-import { LogicRoomReg } from './LogicRoomReg';
-
-document.querySelectorAll('.js-registration-container').forEach((room) => {
-  new LogicRoomReg(room);
-});
--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -2,9 +2,61 @@ import '../../blocks/dropdown/dropdown-init';
 import '../../scss/main.scss';
 import './registration.scss';
 import '../../blocks/burger/burger-init';
-import '../../blocks/logic-room-reg/logic-room-init';
 import '../../blocks/iMask/iMask-init';
-/* проверяю пиксель перфект через свой плагин, после каждого изменения,
-я потом удалю эти пути после проверок */
- //import '../../../pixel.js'
- //import '../../../pixel.css'
+
+ class LogicRoomReg {
+    private room: Element
+  
+    private elements: Element[]
+  
+    constructor(room: Element) {
+      this.room = room;
+      this.elements = this.findElement();
+      this.bindEventListeners();
+    }
+  
+    private findElement(): Element[] {
+      const buttonSignIn = this.room.querySelector('.js-button_signIn')!;
+      const buttonCreateAccount = this.room.querySelector('.js-button_regCreate')!;
+      const formSignIn = this.room.querySelector('.js-registration-container__signing')!;
+      const formCreate = this.room.querySelector('.js-registration-container__reg')!;
+  
+      return [buttonSignIn,
+        buttonCreateAccount,
+        formCreate,
+        formSignIn];
+    }
+  
+    private bindEventListeners() {
+      this.handleSignInButtonClick = this.handleSignInButtonClick.bind(this);
+      this.elements[0].addEventListener('click', this.handleSignInButtonClick);
+  
+      this.handleAccountButtonClick = this.handleAccountButtonClick.bind(this);
+      this.elements[1].addEventListener('click', this.handleAccountButtonClick);
+    }
+  
+    private handleAccountButtonClick() {
+      this.elements[2].classList.toggle('registration-container_active');
+      this.elements[3].classList.remove('registration-container_active');
+      window.history.pushState({
+        param: 'Value',
+      }, '', 'registration.html');
+      this.room.classList.add('registration-container_reg-bg');
+      this.room.classList.remove('registration-container_signIn-bg');
+    }
+  
+    private handleSignInButtonClick() {
+      this.elements[3].classList.toggle('registration-container_active');
+      this.elements[2].classList.remove('registration-container_active');
+      window.history.pushState({
+        param: 'Value',
+      }, '', 'signing.html');
+      this.room.classList.remove('registration-container_reg-bg');
+      this.room.classList.add('registration-container_signIn-bg');
+    }
+  }
+  
+  document.querySelectorAll('.js-registration-container').forEach((room) => {
+    new LogicRoomReg(room);
+  });
+  
